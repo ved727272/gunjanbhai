286e8cea7b86f9814f53c802885773d0ff2e9120
Brijesh Raman <brijesh.zte@gmail.com>
2025-07-05 11:49:18 +0530

    rgaddedagain2

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg2" {
     name = "ramanrgnew1"
     location = "eastus"  
 }
+
+resource "azurerm_resource_group" "rg3" {
+    name = "ramanrgnew2"
+    location = "eastus"  
+}
